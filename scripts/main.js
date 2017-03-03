1697a7b9244138be096b80ce662afce1e4ed5595
Tobi <tobiadeyemi91@gmail.com>
2017-03-03 01:42:33 +0100

    add functionality to load sample fishes

--- scripts/main.js
+++ scripts/main.js
@@ -46,6 +46,12 @@ var App = React.createClass({
     })
   },
 
+  loadSamples: function() {
+    this.setState({
+      fishes: require('./sample-fishes')
+    });
+  },
+
   render: function(){
     return (
       <div className="catch-of-the-day" >
@@ -53,7 +59,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
@@ -128,6 +134,7 @@ var Inventory = React.createClass({
         <h2>Inventory</h2>
 
         <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
 
