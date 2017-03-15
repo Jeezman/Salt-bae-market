6be948c0d334062394ce1eb2b24d3e66023fa8c2
Tobi <tobiadeyemi91@gmail.com>
2017-03-15 17:56:33 +0100

    add order functionality

--- scripts/main.js
+++ scripts/main.js
@@ -35,6 +35,11 @@ var App = React.createClass({
     }
   },
 
+  addToOrder: function(key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order: this.state.order });
+  },
+
   addFish: function(fish) {
     var timestamp = (new Date()).getTime();
     //update state object
@@ -53,7 +58,8 @@ var App = React.createClass({
   },
 
   renderFish: function(key){
-    return <Fish key={key} index={key} details={this.state.fishes[key]} />
+    return <Fish key={key} index={key} details={this.state.fishes[key]} 
+    addToOrder={this.addToOrder} />
   },
 
   render: function(){
@@ -75,8 +81,15 @@ var App = React.createClass({
 
 // Fish component
 var Fish = React.createClass({
+  onButtonClick: function() {
+    console.log('Going to add the fish: ', this.props.index);
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
   render: function() {
     var details = this.props.details;
+    var isAvailable = (details.status === 'available' ? true : false);
+    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!')
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name} />
@@ -85,6 +98,7 @@ var Fish = React.createClass({
         </h3>
         <span className="price">{helpers.formatPrice(details.price)}</span>
         <p>{details.desc}</p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick} >{buttonText}</button>
       </li>
     )
   }
